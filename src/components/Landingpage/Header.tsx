cfe31830b54fb799d21e8651f7fbfe0575c1c52b
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-11-14 17:44:43 +0000

    new upload

--- src/components/Landingpage/Header.tsx
+++ src/components/Landingpage/Header.tsx
@@ -4,23 +4,24 @@ import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-800 text-white p-4">
+    <header className="bg-blue-800 text-white p-5 ">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo Section */}
         <Link href="/">
-          <div className="flex items-center space-x-2 cursor-pointer">
-            <Image
-              src="/machinelog.png" // Correct path to image in /public folder
+          <div className="flex items-center space-x-2 cursor-pointer overflow-hidden">
+            {/* <Image
+              src="/machinelogo.png" // Correct path to image in /public folder
               alt="Samir Bag & Jean Repairing Center Logo"
-              width={40}
-              height={40}
+              width={120}
+              height={100}
               className="rounded-full"
-            />
+            /> */}
+            <h1 className=' text-2xl font-bold capitalize'>samir collections</h1>
           </div>
         </Link>
 
         {/* Navigation Section */}
-        <nav>
+        <nav className='flex items-center gap-5'>
           <ul className="hidden md:flex gap-6">
             <li><Link href="/" className="hover:text-yellow-400 transition-colors">Home</Link></li>
             <li><Link href="/services" className="hover:text-yellow-400 transition-colors">Services</Link></li>
@@ -30,8 +31,10 @@ const Header: React.FC = () => {
           </ul>
           
           {/* Mobile Navigation */}
-          <div className="md:hidden">
-            <button className="text-white">☰</button>
+          <div className="">
+            <Link href="/login">
+            <button className=" px-2 py-1 bg-red-500 rounded-md text-white">Login</button>
+            </Link>
             {/* You can implement a mobile menu here with dropdown or modal */}
           </div>
         </nav>
