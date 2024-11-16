1c8853f161299c96e5b75c553299a831baede08e
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-11-16 17:55:33 +0000

    updated

--- src/components/Landingpage/Servicecard.tsx
+++ src/components/Landingpage/Servicecard.tsx
@@ -8,9 +8,9 @@ interface ServiceCardProps {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, imageSrc }) => {
   return (
-    <div className="p-4 bg-white shadow-md rounded-lg">
+    <div className="p-2 bg-white shadow-md rounded-lg w-[350px]" key={title}>
       <img src={imageSrc} alt={title} className="w-full h-40 object-cover rounded" />
-      <h4 className="mt-4 font-bold">{title}</h4>
+      <h4 className="mt-4 font-bold text-gray-800">{title}</h4>
       <p className="text-gray-600">{description}</p>
     </div>
   );
