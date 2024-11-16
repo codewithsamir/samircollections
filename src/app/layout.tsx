be191be6dca7e1d71f242bac498561047599f974
samir rain <124909580+codewithsamir@users.noreply.github.com>
2024-11-16 18:27:42 +0000

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from '@/components/Landingpage/Header'
 import Footer from '@/components/Landingpage/Footer'
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,10 +16,12 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Samir Collections - Bag & Jean Repairing Center",
-  description: "Samir Bag and Jean Repairing Center in Janakpur offers expert repair and fitting services for bags, jeans, jackets, and more. Convenient home pickup service available.",
-};
+// export const metadata: Metadata = {
+//   title: "Samir Collections - Bag & Jean Repairing Center",
+//   description: "Samir Bag and Jean Repairing Center in Janakpur offers expert repair and fitting services for bags, jeans, jackets, and more. Convenient home pickup service available.",
+//   // Adding google-site-verification meta tag
+//   googleSiteVerification: "Geg6MyqELKWvjbsABit5WRiVwZ9ua-TMkbRUObCVSIA",
+// };
 
 
 export default function RootLayout({
@@ -28,6 +31,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Head>
+        <meta name="google-site-verification" content="Geg6MyqELKWvjbsABit5WRiVwZ9ua-TMkbRUObCVSIA" />
+        <title>Samir Collections - Bag & Jean Repairing Center</title>
+        <meta name="description" content="Samir Bag and Jean Repairing Center in Janakpur offers expert repair and fitting services for bags, jeans, jackets, and more. Convenient home pickup service available." />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
