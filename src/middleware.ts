8411e5e280dd555add146f218b9e1eb900379962
arjuna777hack <arjununlocker@gmail.com>
2024-12-05 09:16:05 +0545

    verifytoken

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/login", "/signup", "/Dashboard/:path*"],
+  matcher: ["/login", "/signup", "/Dashboard/:path*"],
 };
