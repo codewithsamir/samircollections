feea28bbb22c9d9a67abac16d8a11405ca228ba9
Nawaj Sarif <mdnawajn27@gmail.com>
2024-12-06 00:35:09 +0545

    next update

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,9 +9,9 @@ export async function GET(request: NextRequest) {
 
     try {
         const userId = await getDataFromToken(request)
-        console.log(userId)
+        // console.log(userId)
         const user = await CustomerUser.findOne({_id: userId}).select("-password ")
-        console.log(user)
+        // console.log(user)
         return NextResponse.json({
             message:"user found successfully",
             data: user,
