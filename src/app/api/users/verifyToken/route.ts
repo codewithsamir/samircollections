34e1f22582dc61d9e831a6be2648af38927429ed
arjuna777hack <arjununlocker@gmail.com>
2024-12-05 09:16:05 +0545

    verifytoken

---
+++ src/app/api/users/verifyToken/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import CustomerUser from "@/models/user.model";
+import { error } from "console";
+
+connect()
+
+export async function POST(request: NextRequest){
+
+    try {
+        const reqbody = await request.json()
+        const {token} = reqbody;
+        console.log(token)
+      const user = await  CustomerUser.findOne({
+            verifyToken:token,
+            verifyTokenExpiry:{$gt: Date.now()}
+        })
+
+        if(!user){
+            return NextResponse.json({error:"Invalid Token"},{status:400})
+        }
+        console.log(user);
+
+        user.isVerified = true;
+        user.verifyToken = undefined
+        user.verifyTokenExpiry = undefined
+        await user.save()
+        
+        return NextResponse.json({message:"Email verified successfully", success:true})
+        
+    } catch (error) {
+        const typedError = error as Error;
+     return NextResponse.json({error: typedError.message},
+        {status:500}
+     )   
+    }
+}
+
---
+++ src/helpers/mailer.ts
@@ -0,0 +1,62 @@
+import nodemailer from 'nodemailer'
+import CustomerUser  from "@/models/user.model"
+import bcryptjs from 'bcryptjs'
+// Looking to send emails in production? Check out our Email API/SMTP product!
+
+
+
+export const sendemail = async ({ email, emailType, userId }: any) => {
+
+    try {
+        //create a hash token
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+       if(emailType === "VERIFY"){
+        await CustomerUser.findByIdAndUpdate(userId,
+            {
+                verifyToken: hashedToken,
+                verifyTokenExpiry: Date.now() + 3600000
+            }
+           
+
+        )
+       }
+       else if(emailType === "RESET"){
+        await CustomerUser.findByIdAndUpdate(userId,
+            {
+                forgotPasswordToken: hashedToken,
+                forgotPasswordTokenExpiry: Date.now() + 3600000
+            }
+           
+
+        )
+       }
+       
+    //    we have to add this user and pass in env file 
+
+       const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "a5c131a7436756",
+            pass: "67db317945063f"
+        }
+    });
+
+    const mailOptions = {
+        from: 'samirbagandjeansreapairingservice@gmail.com',
+        to: email,
+        subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+    html : `<p> <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify form email" : "reset your passowrd"}</p>` 
+    }
+
+    const mailresponse = await transport.sendMail(mailOptions)
+    return mailresponse;
+
+
+    } catch (error) {
+        const typedError = error as Error;
+        throw new Error(typedError.message)
+
+    }
+}
