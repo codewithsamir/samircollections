7b3f825e02caba4bb27f649dd4d10296a821e067
samir rain <124909580+codewithsamir@users.noreply.github.com>
2025-03-05 09:51:34 +0000

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,15 +80,22 @@ export default function RootLayout({
       }
     `}
   </script>
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JPZ8LS2R2Y"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-JPZ8LS2R2Y');
-</script>
+ {/* Google Tag Manager Script */}
+ <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JPZ8LS2R2Y"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JPZ8LS2R2Y');
+            `,
+          }}
+        />
 </Head>
 
 
