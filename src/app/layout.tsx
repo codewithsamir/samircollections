56ed47989c420c68394384d994b9c9198ec5a435
samir rain <124909580+codewithsamir@users.noreply.github.com>
2025-03-05 09:22:00 +0000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,15 @@ export default function RootLayout({
       }
     `}
   </script>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JPZ8LS2R2Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JPZ8LS2R2Y');
+</script>
 </Head>
 
 
